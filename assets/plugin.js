42dd649f2608e83a58f09e8af19207a7a4dc33f5
David Moreno García <david.mogar@gmail.com>
2017-04-08 00:04:59 +0200

    Add improvements and new feature
    Now is possible to copy the command clicking a button. Also, when hovering, the text fades out to show only the command.

--- assets/plugin.js
+++ assets/plugin.js
@@ -3,7 +3,27 @@ require(['gitbook', 'jQuery'], function(gitbook, $) {
   const TERMINAL_HOOK = '**[terminal]'
 
   var pluginConfig = {};
-  var regex = /\*\*\[(command|delimiter|error|prompt|warning) ((?:[^\]]+|\](?!\*\*|$)|)+)]/
+  var regex = /\*\*\[(command|delimiter|error|path|prompt|warning) ((?:[^\]]+|\](?!\*\*|$)|)+)]/
+  var timeout;
+
+  function addCopyElements(block) {
+    button = $('<i id="terminal-copy" class="fa fa-clone"></i>');
+    button.click(copyCommand);
+    block.append(button);
+
+    $('body').append('<textarea id="terminal-textarea" />');
+  }
+
+  function copyCommand() {
+    kbdCommand = $('kbd.terminal-command');
+    textarea = $('#terminal-textarea');
+    textarea.val(kbdCommand.text());
+    textarea.focus();
+    textarea.select();
+    document.execCommand('copy');
+    kbdCommand.focus();
+    updateCopyButton();
+  }
 
   function initializePlugin(config) {
     pluginConfig = config.terminal;
@@ -11,15 +31,36 @@ require(['gitbook', 'jQuery'], function(gitbook, $) {
 
   function format_terminal_block(block) {
     block.parent('pre').addClass('terminal terminal-' + pluginConfig.style);
+
+    /* Get text and remove hook */
     text = block.html().replace(TERMINAL_HOOK + '\n', '');
     block.html(parse_tokens(text));
-    $('body').append('<textarea id="terminal" />');
+
+    if (pluginConfig.copyButton) {
+      addCopyElements(block);
+    }
   }
 
   function parse_tokens(text) {
     return text.replace(new RegExp(regex, 'gm'), function(match, token, value) {
-      return '<span class="terminal-' + token + '">' + value + '</span>';
-    });
+      element = null;
+
+      if (token == 'command') {
+        element = '<kbd class="terminal-' + token + '">' + value + '</kbd><tt>';
+      } else {
+        element = '<span class="terminal-' + token + '">' + value + '</span>';
+      }
+
+      return element;
+    }) + '</tt>';
+  }
+
+  function updateCopyButton() {
+    $('#terminal-copy').removeClass('fa-clone').addClass('fa-check');
+    clearTimeout(timeout);
+    timeout = window.setTimeout(function() {
+      $('#terminal-copy').removeClass('fa-check').addClass('fa-clone');
+    }, 1000);
   }
 
   gitbook.events.bind('start', function(e, config) {
